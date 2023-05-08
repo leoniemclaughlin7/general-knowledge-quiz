52ca97e9103fe32915f6ee7d1da0864febbfec97
leoniemclaughlin7 <>
2023-05-08 16:56:53 +0000

    Add quiz questions to questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,17 +2,57 @@
 const quizQuestion = [
     {
         question: "What 90s boy band member bought Myspace in 2011?",
-        answer: "1919",
+        answer: "Justin Timberlake",
         options: [
-            "1920",
-            "1917",
-            "1919",
-            "1918"
+            "Nick Lachey",
+            "Justin Timberlake",
+            "Shawn Stockmn",
+          "AJ McLean"
         ]
     },
 
+     {
+        question: "What is the most visited tourist attraction in the world?",
+        answer: "Eiffel Tower",
+        options: [
+            "Eiffel Tower",
+            "Statue of Liberty",
+            "Great Wall of China",
+            "Colosseum"
+        ]
+    },
 
+     {
+        question: "What’s the name of Hagrid’s pet spider?",
+        answer: "Aragog",
+        options: [
+            "Nigini",
+            "Crookshanks",
+            "Aragog",
+            "Mosag"
+        ]
+    },
 
+     {
+        question: "What’s the heaviest organ in the human body?",
+        answer: "Liver",
+        options: [
+            "Skin",
+            "Heart",
+            "Liver",
+            "Brain"
+        ]
+    },
 
+    {
+        question: "Which of these EU countries does not use the euro as its currency?",
+        answer: "All of the above",
+        options: [
+            "Poland",
+            "Denmark",
+            "Sweden",
+            "All of the above"
+        ]
+    },
 
 ];
