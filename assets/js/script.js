3f69c769735f7ffecd7b64075f1cfde2b5dffe01
leoniemclaughlin7 <>
2023-05-09 18:58:42 +0000

    Add showScore function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 let question = document.getElementById('question');
-let answers = document.querySelectorAll('.answers'); 
-let scores = document.getElementById('score');
+let answers = document.querySelectorAll('.answers');
+let scores = document.getElementById("score");
 
 let questionCounter = 0;
 let availableQuestions = [];
 let score = 0;
 
-function showQuestion(quizQ, qCount){
+function showQuestion(quizQ, qCount) {
     question.textContent = quizQ[qCount].question;
-    answers.forEach(function (element, index){
+    answers.forEach(function (element, index) {
         element.textContent = quizQ[qCount].answers[index];
 
-        element.addEventListener('click', function(){
-            if(quizQ[qCount].correctAnswer === index){
+        element.addEventListener('click', function () {
+            if (quizQ[qCount].correctAnswer === index) {
                 element.style.color = 'green';
                 score++;
                 nextQuestion();
-            }else {
+            } else {
                 element.style.color = 'red';
                 nextQuestion();
             }
@@ -26,14 +26,19 @@ function showQuestion(quizQ, qCount){
 
 }
 
-function nextQuestion(){
+function nextQuestion() {
     questionCounter++;
-     if(questionCounter < quizQuestion.length){
-    showQuestion(quizQuestion, questionCounter);
-  }
+    if (questionCounter < quizQuestion.length) {
+        showQuestion(quizQuestion, questionCounter);
+        showScore();
+    }
 }
 
-showQuestion(quizQuestion,0);
+function showScore() {
+    scores.innerHTML = score;
+}
+
+showQuestion(quizQuestion, 0);
 
 
 
